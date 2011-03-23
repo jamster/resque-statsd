0cf66730ca4082d0e2969b5e5e132202ef93d19b
Jason Amster <jayamster@gmail.com>
2011-03-23 01:01:33 -0400

    updated readme and took away puts

--- lib/resque-statsd.rb
+++ lib/resque-statsd.rb
@@ -5,8 +5,6 @@ require 'resque/plugins/statsd'
 # Set up the client
 $resque_statsd = Statsd.new(ENV['GRAPHITE_HOST'], 8125)
 $resque_statsd.namespace="#{ENV['APP_NAME']}_#{ENV['RAILS_ENV']}.resque"
-puts $resque_statsd.namespace
-
 
 
 module Resque
