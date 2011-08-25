e1b9d27bfdce1027dcb8f080ce31ecf0dd22a4d5
Jon Frisby <jfrisby@mrjoy.com>
2011-08-25 13:03:18 -0700

    Hoisting defaults, adding env-search, fixing stupid.

--- lib/resque-statsd.rb
+++ lib/resque-statsd.rb
@@ -7,15 +7,32 @@ module Resque
     module Statsd
       VERSION = "0.0.2"
 
-      attr_accessor :graphite_host
-      attr_accessor :graphite_port
-      attr_accessor :namespace
+      def self.graphite_host
+        return @@graphite_host || ENV['GRAPHITE_HOST'] || 'localhost'
+      end
+      def self.graphite_host=(val)
+        @@graphite_host = val
+      end
+
+      def self.graphite_port
+        return @@graphite_port || ENV['GRAPHITE_PORT'] || 8125
+      end
+      def self.graphite_port=(val)
+        @@graphite_port = val
+      end
+
+      def self.namespace
+        return @@namespace || ENV['GRAPHITE_NAMESPACE'] || 'resque'
+      end
+      def self.namespace=(val)
+        @@namespace = val
+      end
 
       # Set up the client lazily, to minimize order-of-operations headaches.
       def self.statsd
         if(@@stats.nil?)
-          @@statsd = Statsd.new(graphite_host || 'localhost', graphite_port || 8125)
-          @@statsd.namespace = namespace || "resque"
+          @@statsd = Statsd.new(graphite_host, graphite_port)
+          @@statsd.namespace = namespace
         end
         return @@statsd
       end
