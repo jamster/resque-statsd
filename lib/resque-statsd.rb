c7d0793077f31084859296cd19d19734ded16c03
Jon Frisby <jfrisby@mrjoy.com>
2011-08-25 12:43:09 -0700

    Changing to lazy-instantiation model, so we can switch to a more robust configuration model.

--- lib/resque-statsd.rb
+++ lib/resque-statsd.rb
@@ -2,17 +2,23 @@ require 'rubygems'
 require 'statsd' # Really Jnunemakers's mine branch of statsd 
 require 'resque/plugins/statsd'
 
-# Set up the client
-$resque_statsd = Statsd.new(ENV['GRAPHITE_HOST'], 8125)
-$resque_statsd.namespace="#{ENV['APP_NAME']}_#{ENV['RAILS_ENV']}.resque"
-
-
 module Resque
   module Plugins
     module Statsd
+      VERSION = "0.0.2"
 
-      VERSION = "0.0.1"
-      
+      attr_accessor :graphite_host
+      attr_accessor :graphite_port
+      attr_accessor :namespace
+
+      # Set up the client lazily, to minimize order-of-operations headaches.
+      def self.statsd
+        if(@@stats.nil?)
+          @@statsd = Statsd.new(graphite_host || 'localhost', graphite_port || 8125)
+          @@statsd.namespace = namespace || "resque"
+        end
+        return @@statsd
+      end
     end
   end
 end
--- lib/resque/plugins/statsd.rb
+++ lib/resque/plugins/statsd.rb
@@ -3,27 +3,25 @@ module Resque
     module Statsd
 
       def after_enqueue_statsd(*args)
-        $resque_statsd.increment("#{@queue}.enqueued")
-        $resque_statsd.increment("total.enqueued")
-        
+        self.statsd.increment("#{@queue}.enqueued")
+        self.statsd.increment("total.enqueued")
       end
-      
+
       def after_perform_statsd(*args)
-        $resque_statsd.increment("#{@queue}.finished")
-        $resque_statsd.increment("total.finished")
+        self.statsd.increment("#{@queue}.finished")
+        self.statsd.increment("total.finished")
       end
-      
+
       def on_failure_statsd(*args)
-        $resque_statsd.increment("#{@queue}.failed")
-        $resque_statsd.increment("total.failed")
+        self.statsd.increment("#{@queue}.failed")
+        self.statsd.increment("total.failed")
       end
-      
+
       def around_perform_statsd(*args)
-        $resque_statsd.time("#{@queue}.processed") do 
+        self.statsd.time("#{@queue}.processed") do
           yield
         end
       end
-
     end
   end
 end
