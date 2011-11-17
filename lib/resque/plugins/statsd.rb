08850702c0c93124b87b0be323c5bd31c473efd6
Naseem Hakim <naseem@airbnb.com>
2011-11-17 14:28:00 -0800

    Logging split by job as well as queue in statsd

--- lib/resque/plugins/statsd.rb
+++ lib/resque/plugins/statsd.rb
@@ -3,23 +3,28 @@ module Resque
     module Statsd
 
       def after_enqueue_statsd(*args)
-        StatsdHelper.statsd.increment("resque.#{@queue}.enqueued")
+        StatsdHelper.statsd.increment("resque.queues.#{@queue}.enqueued")
+        StatsdHelper.statsd.increment("resque.jobs.#{self.name}.enqueued")
         StatsdHelper.statsd.increment("resque.total.enqueued")
       end
 
       def after_perform_statsd(*args)
-        StatsdHelper.statsd.increment("resque.#{@queue}.finished")
+        StatsdHelper.statsd.increment("resque.queues.#{@queue}.finished")
+        StatsdHelper.statsd.increment("resque.jobs.#{self.name}.finished")
         StatsdHelper.statsd.increment("resque.total.finished")
       end
 
       def on_failure_statsd(*args)
-        StatsdHelper.statsd.increment("resque.#{@queue}.failed")
+        StatsdHelper.statsd.increment("resque.queues.#{@queue}.failed")
+        StatsdHelper.statsd.increment("resque.jobs.#{self.name}.failed")
         StatsdHelper.statsd.increment("resque.total.failed")
       end
 
       def around_perform_statsd(*args)
-        StatsdHelper.statsd.time("resque.#{@queue}.processed") do
-          yield
+        StatsdHelper.statsd.time("resque.queues.#{@queue}.processed") do
+          StatsdHelper.statsd.time("resque.jobs.#{self.name}.processed") do
+            yield
+          end
         end
       end
     end
