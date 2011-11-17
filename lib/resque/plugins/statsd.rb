8614020bfd749bb72e1f177546ec3b0a80b1137d
Naseem Hakim <naseem@airbnb.com>
2011-11-17 14:55:06 -0800

    resque is the default namespace, so removing from module

--- lib/resque/plugins/statsd.rb
+++ lib/resque/plugins/statsd.rb
@@ -3,26 +3,26 @@ module Resque
     module Statsd
 
       def after_enqueue_statsd(*args)
-        StatsdHelper.statsd.increment("resque.queues.#{@queue}.enqueued")
-        StatsdHelper.statsd.increment("resque.jobs.#{self.name}.enqueued")
-        StatsdHelper.statsd.increment("resque.total.enqueued")
+        StatsdHelper.statsd.increment("queues.#{@queue}.enqueued")
+        StatsdHelper.statsd.increment("jobs.#{self.name}.enqueued")
+        StatsdHelper.statsd.increment("total.enqueued")
       end
 
       def after_perform_statsd(*args)
-        StatsdHelper.statsd.increment("resque.queues.#{@queue}.finished")
-        StatsdHelper.statsd.increment("resque.jobs.#{self.name}.finished")
-        StatsdHelper.statsd.increment("resque.total.finished")
+        StatsdHelper.statsd.increment("queues.#{@queue}.finished")
+        StatsdHelper.statsd.increment("jobs.#{self.name}.finished")
+        StatsdHelper.statsd.increment("total.finished")
       end
 
       def on_failure_statsd(*args)
-        StatsdHelper.statsd.increment("resque.queues.#{@queue}.failed")
-        StatsdHelper.statsd.increment("resque.jobs.#{self.name}.failed")
-        StatsdHelper.statsd.increment("resque.total.failed")
+        StatsdHelper.statsd.increment("queues.#{@queue}.failed")
+        StatsdHelper.statsd.increment("jobs.#{self.name}.failed")
+        StatsdHelper.statsd.increment("total.failed")
       end
 
       def around_perform_statsd(*args)
-        StatsdHelper.statsd.time("resque.queues.#{@queue}.processed") do
-          StatsdHelper.statsd.time("resque.jobs.#{self.name}.processed") do
+        StatsdHelper.statsd.time("queues.#{@queue}.processed") do
+          StatsdHelper.statsd.time("jobs.#{self.name}.processed") do
             yield
           end
         end
